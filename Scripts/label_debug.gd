c0a448c3cea040a7ecd34fee132d9f08f91e148b
Artem Kozin <akozin@cbr13s1.42barcelona.com>
2025-12-04 13:36:04 +0100

    labels for checking rotations in debug, misc rearrangements in 3d space

---
+++ Scripts/label_debug.gd
@@ -0,0 +1,16 @@
+extends Label
+
+@onready var shadow_caster: CharacterBody3D = $"../ShadowCaster"
+
+func _process(delta: float) -> void:
+	self.text = "
+rotx: %8.2f | %8.2f
+roty: %8.2f | %8.2f
+rotz: %8.2f | %8.2f" % [
+	shadow_caster.rotation.x,
+	shadow_caster.rotation.x / PI,
+	shadow_caster.rotation.y,
+	shadow_caster.rotation.y / PI,
+	shadow_caster.rotation.z,
+	shadow_caster.rotation.z / PI
+];
