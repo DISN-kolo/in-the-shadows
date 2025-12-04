70f6800c5707792efec02b18e52b35e45686d9ed
Artem Kozin <akozin@cbr13s1.42barcelona.com>
2025-12-04 15:47:36 +0100

    made meshes into their own scenes, added tmp dummy mesh loader

--- Scripts/ShadowCaster.cs
+++ Scripts/ShadowCaster.cs
@@ -7,10 +7,17 @@ public partial class ShadowCaster : CharacterBody3D
 	public Vector2 Target = new Vector2(0, 0);
 	public Vector2 ScreenSize = new Vector2(0, 0);
 
+	public PackedScene TempVarForModel;
+	public Node InstanceOfTempModel;
+
 	private Vector2 CalculateAngle()
 	{
-		Vector2 res = new Vector2((float)Target.X / (float)ScreenSize.X * (float)Math.PI * (float)Settings.MouseSens,
-				(float)Target.Y / (float)ScreenSize.Y * (float)Math.PI * (float)Settings.MouseSens);
+		Vector2 res = new Vector2(
+			(float)Target.X /
+				(float)ScreenSize.X * (float)Math.PI * (float)Settings.MouseSens,
+			(float)Target.Y /
+				(float)ScreenSize.Y * (float)Math.PI * (float)Settings.MouseSens
+		);
 		return res;
 	}
 
@@ -24,38 +31,39 @@ public partial class ShadowCaster : CharacterBody3D
 	public override void _Ready()
 	{
 		ScreenSize = GetViewport().GetVisibleRect().Size;
+		TempVarForModel = GD.Load<PackedScene>("res://Scenes/MeshScenes/ElephantMesh.tscn");
+		InstanceOfTempModel = TempVarForModel.Instantiate();
+		AddChild(InstanceOfTempModel);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		Rotation = Rotation with {
-			Y = (float)Mathf.Lerp(Rotation.Y, CalculateAngle().X, delta * Settings.RotateVel),
-			X = (float)Mathf.Lerp(Rotation.X, CalculateAngle().Y, delta * Settings.RotateVel)};
+			Y = (float)Mathf.Lerp(
+				Rotation.Y, CalculateAngle().X, delta * Settings.RotateVel
+			),
+			X = (float)Mathf.Lerp(
+				Rotation.X, CalculateAngle().Y, delta * Settings.RotateVel
+			)
+		};
 	}
 
 	public override void _Input(InputEvent @event)
 	{
 		if (Input.IsActionJustPressed("LMB"))
 		{
-			GD.Print("action just pressed lmb during _input handling");
 			LMBDown = true;
 		}
 		else if (Input.IsActionJustReleased("LMB"))
 		{
-			GD.Print("action just released lmb during _input handling");
 			LMBDown = false;
 		}
 		if (@event is InputEventMouseMotion eventMouseMotion)
 		{
-			GD.Print("Mouse Motion at: ", eventMouseMotion.Position);
-			GD.Print("Event relative: ", eventMouseMotion.Relative);
 			if (LMBDown)
 			{
 				Target += eventMouseMotion.Relative;
-				GD.Print("Target updated to: ", Target);
 			}
 		}
-//		GD.Print("Viewport Resolution is: ", GetViewport().GetVisibleRect().Size);
 	}
 }
